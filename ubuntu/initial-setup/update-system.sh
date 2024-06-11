2925774414a63657c60d47b5e03f112777d0975b
kayufok <kayufok91@gmail.com>
2024-06-11 21:36:27 +0800

    Update update-system.sh

--- ubuntu/initial-setup/update-system.sh
+++ ubuntu/initial-setup/update-system.sh
@@ -1,6 +1,6 @@
-#!/bin/bash 
+#!/bin/bash
 ###########################################################################
-# 
+#
 #   1. updates the list of available packages and their versions
 #   2. installs newer versions of the packages
 #   3. Setup ufw
@@ -8,54 +8,43 @@
 ###########################################################################
 
 # 1. updates the list of available packages and their versions
-sudo apt-get update;
+sudo apt update
 
 # 2. installs newer versions of the packages
-sudo apt-get upgrade -y;
+sudo apt full-upgrade -y
 
 # 3. Setup UFW
-sudo apt install ufw -y;
+sudo apt install ufw -y
 
-sudo ufw default deny incoming;
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw allow ssh
+sudo ufw enable
 
-sudo ufw default allow outgoing;
+# 4. Setup fail2ban to block IPs after failed attempts
+sudo apt install fail2ban -y
 
-sudo ufw allow ssh;
+# fail2ban local config
+sudo cat <<EOT > /etc/fail2ban/jail.local
+[DEFAULT]
+ignoreip = 127.0.0.1/8 999.999.999.0/24 888.888.888.0/24
 
-sudo ufw allow 22;
-
-sudo ufw enable;
-
-# 7. Setup fail2ban to block ip after failed attempts
-sudo apt-get install fail2ban -y;
-
-# fail2ban local config 
-
-printf '%s\n' \
-'[DEFAULT]' \
-'ignoreip = 127.0.0.1/8 999.999.999.0/24 888.888.888.0/24' \
-'' \
-'[sshd]' \
-'enabled  = true' \
-'port     = ssh' \
-'filter   = sshd' \
-'logpath  = /var/log/auth.log' \
-'# backend = systemd' \
-'maxretry = 5' \
-'findtime = 86400' \
-'bantime  = 864000' > /etc/fail2ban/jail.local
+[sshd]
+enabled  = true
+port     = ssh
+filter   = sshd
+logpath  = /var/log/auth.log
+# backend = systemd
+maxretry = 5
+findtime = 86400
+bantime  = 864000
+EOT
 
 # restart fail2ban
-systemctl restart fail2ban
+sudo systemctl restart fail2ban
 
-# checkban list
-#
-#
-#
-fail2ban-client status sshd
+# check Fail2Ban status for sshd
+sudo fail2ban-client status sshd
 
-# check login hist 
-#
-#
-#
+# check login history
 tail -n 100 /var/log/auth.log
